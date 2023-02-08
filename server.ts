e174f1ef30d9129ff2a7eba84c940a9cf0ec5dec
DHRUVRAJSINH <dhruvsss007@gmail.com>
2023-02-08 15:07:28 +0530

    feat: configuration of eslint and prettier fiexd

--- server.ts
+++ server.ts
@@ -1,21 +1,21 @@
-import express from "express";
-import { code } from "./services/openai";
-import bodyParser from "body-parser";
-import { Request, Response, ErrorRequestHandler } from "express";
-import cors from "cors";
+import express from 'express';
+import { code } from './services/openai';
+import bodyParser from 'body-parser';
+import { Request, Response, ErrorRequestHandler } from 'express';
+import cors from 'cors';
 
 export const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use("/code", code);
+app.use('/code', code);
 app.use(cors()); // configure cors
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   return res.status(200).send({
     statusCode: 200,
     response: {
-      data: "health check",
+      data: 'health check',
     },
   });
 });
