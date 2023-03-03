f5a3adf8ad89ac94a9fc3a4fa009720ffc52a73c
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2023-03-03 12:50:47 +0530

    fix(dx): error handling init

--- services/Imageai.ts
+++ services/Imageai.ts
@@ -8,13 +8,23 @@ const router = express.Router();
 router.post("/image", async (req: Request, res: Response) => {
   try {
     const { prompt, n, size } = req.body;
-    logger.info("POST: /generate/image", { endpoint: "/generate/image", prompt, n, size });
+    logger.info("POST: /generate/image", {
+      endpoint: "/generate/image",
+      prompt,
+      n,
+      size,
+    });
     const response = await openai.createImage({
       prompt,
       n,
       size,
     });
-    res.status(200).send(response.data);
+
+    if (!response.status.toString().startsWith("2")) {
+      throw new Error("Error while Fetching Images");
+    }
+
+    return res.status(200).send(response.data);
   } catch (err) {
     res.status(500).send(err);
   }
